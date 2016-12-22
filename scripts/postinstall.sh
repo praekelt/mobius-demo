0d396156c11aa5f8fd141a421ba3382f0a5103dc
hedleyroos <hedleyroos@gmail.com>
2016-12-22 10:29:26 +0200

    See if we can do nvm on the build server

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,6 @@
 manage="${VENV}/bin/python ${INSTALLDIR}/${REPO}/manage.py"
 
 $manage migrate --noinput --settings=project.settings
-chown -R ubuntu:ubuntu /var/praekelt/mobius-demo
-su - ubuntu -c "source /home/ubuntu/.nvm/nvm.sh && cd /var/praekelt/mobius-demo &&  nvm install && npm install && npm run build"
+#chown -R ubuntu:ubuntu /var/praekelt/mobius-demo
+#su - ubuntu -c "source /home/ubuntu/.nvm/nvm.sh && cd /var/praekelt/mobius-demo &&  nvm install && npm install && npm run build"
 $manage collectstatic --noinput --settings=project.settings
