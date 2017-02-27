f70c53ebfd5afac810ca0b11411a4fe7bf5ac307
hedleyroos <hedleyroos@gmail.com>
2017-02-27 10:37:55 +0200

    Latest Mobius skeleton pulled in

--- demo/urls.py
+++ demo/urls.py
@@ -1,18 +1,11 @@
 from django.conf.urls import url, include
 from django.views.generic.base import TemplateView
 
-from demo.views import TestView
-
 
 urlpatterns = [
     url(
         r"^$",
         TemplateView.as_view(template_name="demo/home.html"),
         name="home"
-    ),
-    url(
-        r"^test-view/$",
-        TestView.as_view(),
-        name="test-view"
     )
 ]
