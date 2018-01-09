2e8f5f86007968b3dfe44b82d20b285ed95e152e
hedleyroos <hedleyroos@gmail.com>
2018-01-09 15:42:53 +0200

    Run supervisor in foreground so container remains alive

--- docker/services.sh
+++ docker/services.sh
@@ -3,7 +3,6 @@
 /etc/init.d/rabbitmq-server start
 /etc/init.d/redis-server start
 /etc/init.d/memcached start
-/etc/init.d/supervisor start
 /etc/init.d/varnish start
 /etc/init.d/nginx start
-python manage.py runserver 0.0.0.0:8000
+/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
